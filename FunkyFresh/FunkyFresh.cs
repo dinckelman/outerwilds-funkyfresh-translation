1bf051305a76d471287580353a677101820cdf49
Ivan Sosnov <ivanmsosnov@gmail.com>
2023-06-15 01:30:10 +0200

    Initial release

---
+++ FunkyFresh/FunkyFresh.cs
@@ -0,0 +1,20 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace FunkyFresh
+{
+    public class FunkyFresh : ModBehaviour
+    {
+        public static FunkyFresh Instance;
+        private void Awake() {}
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"{nameof(FunkyFresh)} localization is loaded!", MessageType.Success);
+            
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "FunkyFresh", "assets/funkyfresh.xml");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Outer Wilds Stranger Localization Mod
+
+Localization for the English version of Outer Wilds, and the Eye of the Universe DLC. Simply changes all references to The Stranger, and renames it to The Funky Fresh Friend.
+
+This is a reference to a running gag in [FrogMuseum's stream](https://twitch.tv/frogmuseum)
